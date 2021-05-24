2bc439fe3baf05bef159005ff12842c7df432214
Adriel Souza <78874948+A-souza96@users.noreply.github.com>
2021-05-24 18:54:17 -0300

    almost done

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -3,11 +3,17 @@ package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -15,14 +21,50 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	//return all users
 	public List<User> findAll(){
 		return repository.findAll();
 		}
 	
+	//returns a user
 	public User findByid(Long id) {
 		Optional<User> obj =repository.findById(id);
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage()) ;
+		}
+	}
+	
+	public User update(Long id, User obj) {
+		try {
+		User entity = repository.getOne(id);
+		updateData(entity,obj);
+		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
-		return obj.get();
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
 	}
 
 }
